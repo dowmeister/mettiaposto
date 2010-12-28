04e4438e720d705ad6c80dbe8852a60a2377bff6
shardick <shardick@gmail.com>
2010-12-28 16:59:22 +0100

    small issues fixed and some code added

--- FixMi.Frontend/Ajax/Upload.aspx.cs
+++ FixMi.Frontend/Ajax/Upload.aspx.cs
@@ -8,6 +8,8 @@ using FixMi.Framework.Core.Base;
 using System.IO;
 using Jayrock.Json;
 using System.Configuration;
+using FixMi.Framework.Core;
+using FixMi.Framework.Core.Utility;
 
 namespace FixMi.Frontend.Ajax
 {
@@ -21,9 +23,37 @@ namespace FixMi.Frontend.Ajax
 
                 try
                 {
+                    string path = Server.MapPath(Settings.UploadPath);
+                    string originalPath = Path.Combine(path, UploadPaths.Original);
+                    string mobilePath = Path.Combine(path, UploadPaths.Mobile);
+                    string bigPath = Path.Combine(path, UploadPaths.Big);
+                    string smallPath = Path.Combine(path, UploadPaths.Small);
+                    string commentsPath = Path.Combine(path, UploadPaths.Comments);
+
+                    CreateDirectories(path);
+
                     HttpPostedFile file = GetFileFromRequest("fuFile");
+
                     string fileName =  Guid.NewGuid() + Path.GetExtension(file.FileName);
-                    file.SaveAs(Path.Combine(Server.MapPath(ConfigurationManager.AppSettings["UploadPath"]), fileName));
+
+                    System.Drawing.Image originalImage = System.Drawing.Image.FromStream(file.InputStream);
+                    System.Drawing.Image smallImage = WebUtils.ResizeImage(originalImage, 350, 300, true);
+                    System.Drawing.Image mobileImage = WebUtils.ResizeImage(originalImage, 200, 300, true);
+                    System.Drawing.Image commentsImage = WebUtils.ResizeImage(originalImage, 300, 300, true);
+                    System.Drawing.Image bigImage = WebUtils.ResizeImage(originalImage, 640, 480, true);
+
+                    originalImage.Save(Path.Combine(originalPath, fileName));
+                    bigImage.Save(Path.Combine(bigPath, fileName));
+                    mobileImage.Save(Path.Combine(mobilePath, fileName));
+                    commentsImage.Save(Path.Combine(commentsPath, fileName));
+                    smallImage.Save(Path.Combine(smallPath, fileName));
+
+                    originalImage.Dispose();
+                    smallImage.Dispose();
+                    mobileImage.Dispose();
+                    commentsImage.Dispose();
+                    bigImage.Dispose();
+
                     ret["fileName"] = fileName;
                 }
                 catch (Exception ex)
@@ -33,6 +63,18 @@ namespace FixMi.Frontend.Ajax
 
                 Response.Write(ret.ToString());
             }
+        }
+
+        private void CreateDirectories(string path)
+        {
+            if (!Directory.Exists(Path.Combine(path, UploadPaths.Original)))
+                Directory.CreateDirectory(Path.Combine(path, UploadPaths.Original));
+
+            if (!Directory.Exists(Path.Combine(path, UploadPaths.Mobile)))
+                Directory.CreateDirectory(Path.Combine(path, UploadPaths.Mobile));
+
+            if (!Directory.Exists(Path.Combine(path, UploadPaths.Small)))
+                Directory.CreateDirectory(Path.Combine(path, UploadPaths.Small));
         }
     }
 }
